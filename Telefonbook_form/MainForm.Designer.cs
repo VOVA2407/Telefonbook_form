9008ed2172200105ff776580456cd19558230f23
VOVA2407 <fanrammstein@bk.ru>
2017-05-25 16:41:20 +0300

    adde method to classs telefonist

---
+++ Telefonbook_form/MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 23.05.2017
+ * Time: 11:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Telefonbook_form
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(61, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(61, 79);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Записать";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(43, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Имя";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 160);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Сохранённое имя";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(61, 186);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Вывести";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 229);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(162, 229);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "label4";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(172, 186);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "label5";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(187, 124);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "label6";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "Telefonbook_form";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ Telefonbook_form/MainForm.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 23.05.2017
+ * Time: 11:17
+ * Программа для тестирования и отработки навыков.
+ * 25.05 Создан класс оператор
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+///ust a comment
+namespace Telefonbook_form
+{
+	
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	/// 
+	public class telefonist
+	{
+		//человек, который работает с телефонной книгой
+		private string name;
+		private string number;
+		
+		public string Name()
+		{
+			return name;
+		}
+		
+		public void setName(string Name)
+		{
+			this.name = Name;
+		}
+		
+		public void setNumber(int Number)
+		{
+			this.number = Number;
+		}
+		
+	}
+	public class PhoneBook
+	{
+		private string name; // закрытые поля
+		private string surname;
+		private string number;
+		static private int age = 24;
+		public PhoneBook(string Name)
+		{
+			this.name = Name;
+		}
+
+     public string Familia // свойство
+     {  get { return surname; }
+         set { surname = value; }
+     }
+		public void setSurname(string Surname)
+		{
+			surname = Surname;
+		}
+		public string M1() // 
+		{
+			return name;
+		}
+		public string M2()
+		{
+			return surname;
+		}
+
+		/*public void setName()
+		{get return; set Name = value;}*/
+	}//end of class PhoneBook
+	public partial class MainForm : Form
+	{
+		public static bool rus = false;
+	public static bool eng = false;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+
+		
+		
+		
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+			
+			if ((e.KeyChar > 'А') && (e.KeyChar < 'Я') || (e.KeyChar == '\b'))
+			{
+				rus = true;
+				eng = false;
+				return;
+			}
+			if((e.KeyChar > 'A') && (e.KeyChar < 'Z') || (e.KeyChar == '\b'))
+			{
+				rus = false;
+				eng = true;
+				return;
+			}
+			e.Handled = true;
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+		
+			string Name;
+			Name = textBox1.Text;
+			
+			PhoneBook Book = new PhoneBook(Name);
+			Book.Familia = "VOVA";
+			if(rus)
+			{
+			label3.Text = Book.M1();
+			}
+			if(eng)
+			{
+			label4.Text = Book.M1();
+			}
+			label5.Text = Book.Familia;
+			Book.setSurname("IAKOVLEV");
+			label6.Text = Book.M2();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
